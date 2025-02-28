ded2621eb8233ee8baead6c432029129ac86adec
jpirir-2020285 <jpirir-2020285@kinal.edu.gt>
2025-02-27 22:36:38 -0600

    Se creo estructura del proyecto

---
+++ middlewares/rate.limit.js
@@ -0,0 +1,13 @@
+
+import rateLimit from "express-rate-limit"
+
+export const limiter = rateLimit(
+    {
+        windowMs: 15 * 60 * 1000,
+        max: 100,
+        message: {
+            message: "You'r blocked, wait 15 minutes"
+        }
+
+    }
+)
