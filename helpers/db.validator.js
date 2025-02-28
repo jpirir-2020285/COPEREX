d29d54fc32ed113dcf2e09cc91d7ec7d1708d781
jpirir-2020285 <jpirir-2020285@kinal.edu.gt>
2025-02-27 23:00:00 -0600

    Se crea validacion para usuario unico

---
+++ helpers/db.validator.js
@@ -0,0 +1,11 @@
+import User from '../src/user/user.model.js'
+
+export const existUsername = async(username)=>{
+    const alreadyUsername = await User.findOne({username})
+    if(alreadyUsername){
+        console.error(`Username ${username} is already taken`)
+        throw new Error(`Username ${username} is already taken`)
+    }
+}
+
+
