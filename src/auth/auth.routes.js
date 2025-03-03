e559c5d21d80856fe07605fe4954e5e473fd7080
jpirir-2020285 <jpirir-2020285@kinal.edu.gt>
2025-03-02 21:05:46 -0600

    Se terminaron de agregar validaciones

--- src/auth/auth.routes.js
+++ src/auth/auth.routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express'
 import { login } from './auth.controller.js'
+import { loginValidator } from '../../helpers/validators.js'
 
 const api = Router()
 
 
 api.post(
     '/login',
+    [loginValidator],
     login
 )
 
