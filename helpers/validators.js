eae2ff8fff98b2785c05bd64f2065debfb8a925a
jpirir-2020285 <jpirir-2020285@kinal.edu.gt>
2025-03-02 21:05:28 -0600

    Se terminaron de agregar validaciones

--- helpers/validators.js
+++ helpers/validators.js
@@ -12,3 +12,10 @@ export const addCompanyValidator = [
     body("contact", "Contact is required").notEmpty().withMessage("Contact must be a valid email or phone number"),
     body("yearsOfExperience", "Years of experience is required and must be a number").notEmpty().withMessage("Must be a valid number")
 ]
+
+export const updateCompanyValidator = [
+    body("name", "Company name cannot be empty").notEmpty().isLength({ max: 25 }).withMessage("Name cannot exceed 25 characters"),
+    body("description", "Description is required").notEmpty().isLength({ max: 100 }).withMessage("Description cannot exceed 100 characters"),
+    body("category", "Category is required").notEmpty().isLength({ max: 25 }).withMessage("Category cannot exceed 25 characters"),
+    body("contact", "Contact is required").notEmpty().withMessage("Contact must be a valid email or phone number")
+]
